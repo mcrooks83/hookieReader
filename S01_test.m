2ffe9404dff5c062abcf53ea636c695e1664717b
Timo Rantalainen <tjrantal@gmail.com>
2018-12-14 12:19:49 +0200

    Updated to work with octave

--- S01_test.m
+++ S01_test.m
@@ -22,8 +22,18 @@ sdf = javaObject('java.text.SimpleDateFormat','yyyy-MM-dd HH:mm:ss'); %Create si
 javaMethod('setTimeZone',sdf,tZone);
 jDate = javaObject('java.util.Date'); 
 
-data = readtable('output/59.csv');  %Pop your output file into here
-jDate.setTime(data.tStamp(1));
+if exist('OCTAVE_VERSION', 'builtin')
+	data = dlmread('output/59.csv');  %Pop your output file into here
+	resultant = sqrt(sum(data(:,3:5).^2,2));
+	timeStamps = data(:,2);
+	jDate.setTime(data(1,2));
+else
+	data = readtable('output/59.csv');  %Pop your output file into here
+	resultant = sqrt(sum([data.X data.Y data.Z].^2,2));
+	timeStamps = data.tStamp;
+	jDate.setTime(data.tStamp(1));
+end
+
 if exist('OCTAVE_VERSION', 'builtin')
     dateString = sdf.format(jDate);
     sampleRateString = strsplit(hReader.header.getSampleRate(),'Hz');
@@ -34,12 +44,13 @@ end
 disp(dateString);
 sampleRate = str2num(sampleRateString{1});
 
-resultant = sqrt(sum([data.X data.Y data.Z].^2,2));
+
 [buffered, ignore] = buffer(resultant,sampleRate,0,'nodelay');
-[tStamps, ignore] = buffer(data.tStamp,sampleRate,0,'nodelay');
+[tStamps, ignore] = buffer(timeStamps,sampleRate,0,'nodelay');
 meanVals = mean(buffered,1);
 meanShifted = bsxfun(@minus,buffered,meanVals);
 mads = mean(abs(meanShifted));
 tStamps = tStamps(1,:);
 
-figure,plot(tStamps-tStamps(1),mads)
+fh = figure,plot(tStamps-tStamps(1),mads)
+waitfor(fh);
